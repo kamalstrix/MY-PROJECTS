03b1a04574611aff1ef2b434c9f2af9e8ebab1a1
kamalstrix <91865675+kamalstrix@users.noreply.github.com>
2021-10-05 11:30:56 +0530

    style added

--- js/custom.js
+++ js/custom.js
@@ -61,3 +61,65 @@ $('.slick-one').slick({
 
 
 
+
+
+var $st = $('.pagination');
+var $slickEl = $('.rotation-slider');
+
+$slickEl.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $st.text(i + ' of ' + slick.slideCount);
+});
+
+$slickEl.slick({
+  centerMode: true,
+  centerPadding: '200px',
+  slidesToShow: 1,
+  focusOnSelect: true,
+  dots: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+/*************/
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav',
+	 autoplay: true
+});
+$('.slider-nav').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true
+
+});
+		
+/***********/
+
