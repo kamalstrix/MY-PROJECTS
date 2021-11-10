1c0c32d173f8917646f8c1e51fe4edd9353b8c81
kamalstrix <91865675+kamalstrix@users.noreply.github.com>
2021-11-10 16:42:42 +0530

    style added

--- body by luana/js/custom.js
+++ body by luana/js/custom.js
@@ -1,3 +1,39 @@
+$('.slick-two').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 50000,
+  centerMode:false,
+  infinite:true,
+  dots: true,
+  arrow:false,
+  // prevArrow: '<div class="priv_arrow_one"> <i class="fas fa-long-arrow-alt-left"></i></div>',
+  // nextArrow: '<div class="priv_arrow_two"> <i class="fas fa-long-arrow-alt-right"></i></div>',
+ 
+  
+ 
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arrow:false,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+        arrow:false,
+      }
+    }
+
+  
+  ]
+});
+
 
 
 $('.slick-one').slick({
